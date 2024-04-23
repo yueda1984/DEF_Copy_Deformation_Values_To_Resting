9fe765c5ceab42f03214a0ee038e09ae7b4626e8
yueda1984 <61306261+yueda1984@users.noreply.github.com>
2024-04-23 18:14:31 +0900

    Folder structure update

--- src/DEF_Copy_Deformation_Values_To_Resting.js
+++ src/DEF_Copy_Deformation_Values_To_Resting.js
@@ -12,8 +12,8 @@
 	2) Locate to your user scripts folder (a hidden folder):
 	   https://docs.toonboom.com/help/harmony-17/premium/scripting/import-script.html	
 	   
-	3) Add all unzipped files (*.js, and script-icons folder) directly to the folder above.
-	4) Add DEF_Copy_Deformation_Values_To_Resting to any toolbar.	
+	3) There is a folder named "src" inside the zip file. Copy all its contents directly to the folder above.
+	4) In Harmony, add DEF_Copy_Deformation_Values_To_Resting function to any toolbar.	
 	
 	
 	Direction:
